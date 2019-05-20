96c1fb2f8f745a08db03b64bb494bc07070e75f4
Tyler <tyler1216@gmail.com>
2019-05-19 23:38:17 -0500

    Finished up to challenge 4

---
+++ challenge2.py
@@ -0,0 +1,26 @@
+# Question 2
+# Level 1
+
+# Question:
+# Write a program which can compute the factorial of a given numbers.
+# The results should be printed in a comma-separated sequence on a single line.
+# Suppose the following input is supplied to the program:
+# 8
+# Then, the output should be:
+# 40320
+
+number = int(input("What number? "))
+total = number
+for i in range((number-1), 1, -1):
+    total *= i
+
+print('Total = ' + str(total))
+
+#other solution
+def fact(x):
+    if x == 0:
+        return 1
+    return x * fact(x - 1)
+
+x=int(input())
+print (fact(x))
---
+++ challenge4.py
@@ -0,0 +1,21 @@
+# Question 4
+# Level 1
+
+# Question:
+# Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
+# Suppose the following input is supplied to the program:
+# 34,67,55,33,12,98
+# Then, the output should be:
+# ['34', '67', '55', '33', '12', '98']
+# ('34', '67', '55', '33', '12', '98')
+
+txt = input('Enter string: ')
+
+x = txt.split(',')
+y = tuple(x)
+
+print(x[:2])
+print(x[2:])
+print(x[:-2])
+print(x[0:2])
+print(y)
